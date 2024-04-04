1ca5100db9dfcf75168b26bda993a89fd9cd77e0
Bryan Danquah <bryanda@stlghana.com>
2024-04-04 19:05:57 +0100

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  
+  String _scanBarcodeResult = '';
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(onPressed: scanBarcodeNormal, child: Text("Start Barcode Scanner")),
+            Text("Barcode Result $_scanBarcodeResult")
+          ],
+        ),
+      ),
+    );
+  }
+
+  void scanBarcodeNormal() async{
+    String barcodeScanRes;
+    try{
+      barcodeScanRes=await FlutterBarcodeScanner.scanBarcode(
+      "#ff6666",
+      "cancel",
+      true,
+      ScanMode.BARCODE
+    ); 
+    }  on PlatformException {
+      barcodeScanRes = "Failed to get platform version";
+    }
+    setState(() {
+      _scanBarcodeResult = barcodeScanRes;
+    });
+  }
+}
